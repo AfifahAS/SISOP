3df4a844450b02d491138f46e1b5b0fb7982a2a4
AfifahAS <afifahasmarsari@ymail.com>
2015-11-11 17:17:44 +0700

    Soal 1 Praktikum 1

--- 1.c
+++ 1.c
@@ -1,39 +1,81 @@
 #include<stdio.h>
+#include<unistd.h>
 #include<string.h>
 #include<stdlib.h>
+#include<signal.h>
+
+int execute(char** subcommand);
+void signal_handler(int signum);
 
 int main()
 {
 	char command[100];
-	char program[100];
-	char *subcommand;
-	char *perintah[100];
-	char *path = "/bin";
-	int i = 0, pid = fork();
+	char *subcommand[20];
+	int position;
 
 	while(1)
 	{
-		printf("EO7 >> ");
-		scanf("%[^\n]",command);
+		printf("E07>> ");
 
-		if(strcmp(command,"exit")==0) return 0;
+		signal(SIGINT,signal_handler);
+                signal(SIGTSTP,signal_handler);
 
-		subcommand = strtok(command," ");
-		while(subcommand!=NULL)
+		if(fgets(command,100,stdin)==NULL)
 		{
-			perintah[i] = subcommand;
-			subcommand = strtok(NULL," ");
-			i++;
+			printf("\n");
+			break;
 		}
 
-		strcpy(program,path);
-		strcat(program,perintah[0]);
+		if(command[strlen(command)-1]=='\n') command[strlen(command)-1]='\0';
 
-		for(i=0;i<strlen(program);i++) if(program[i]=='\n') program[i]='\0';
+		subcommand[position]=strtok(command," ");
+		for(position=1;position<20;position++)
+		{
+			subcommand[position] = strtok(NULL," ");
+			if(subcommand[position]==NULL) break;
+		}
 
-		if(pid==0) execvp(program,perintah);
-		else wait(NULL);
-
-		return 0;
+		if(execute(subcommand)==0) break;
 	}
+	return 0;
+}
+
+int execute(char** subcommand)
+{
+        if(strcmp(subcommand[0],"cd")==0)
+        {
+                if(subcommand[1]==NULL) printf("No argument\n");
+		else if(chdir(subcommand[1])) fprintf(stderr,"\n");
+                else chdir(subcommand[1]);
+		return 0;
+        }
+        else
+        {
+                pid_t pid = fork();
+                if(pid==0)
+                {
+                        execvp(subcommand[0], subcommand);
+                        return 0;
+                }
+                else if(pid<0)
+                {
+                        printf("ERROR\n");
+                        return 1;
+                }
+                else
+                {
+                        if(strcmp(subcommand[2],"&")==0);
+                        else
+                        {
+                                int status;
+                                waitpid(pid,&status,0);
+                        }
+                        return 1;
+                }
+	}
+}
+
+void signal_handler(int signum)
+{
+	fprintf(stderr,"\nE07>> ");
 }
